8830e9d0e2d8d413998247967b082a2405c7035f
levonlin <levonlin1@gmail.com>
2017-02-08 14:48:48 +0800

    update

--- autoSync.sh
+++ autoSync.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-git add *.sublime-settings
-git add *.sublime-build
-git add "Default (OSX).sublime-keymap"
+git add *.sublime-*
 git add autoSync.sh
 git commit -m 'update'
 git push origin mac
